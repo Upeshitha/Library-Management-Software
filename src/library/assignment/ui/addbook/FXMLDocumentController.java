58a268ccf534b037f5aafddec8ec624e58d678bc
Eranda Upeshitha <upeshitha@gmail.com>
2021-03-02 21:42:58 +0530

    create first add book UI design

---
+++ src/library/assignment/ui/addbook/FXMLDocumentController.java
@@ -0,0 +1,54 @@
+/**
+* The FXMLDocumentController program control
+* the FXML Document program.
+*
+* @author  Eranda Upeshitha
+* @version 1.0
+* @since   2021-03-02 
+*/
+package library.assignment.ui.addbook;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLDocumentController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private TextField title;
+    @FXML
+    private TextField id;
+    @FXML
+    private TextField author;
+    @FXML
+    private TextField publisher;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button cancelButton;
+    
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void addBook(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) {
+    }
+    
+}
